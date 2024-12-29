32893d13802803b466faddadaced2fa77df7c220
labitayoub <ayoub.labite@gmail.com>
2024-12-29 04:09:03 +0100

    update

---
+++ src/Film.php
@@ -0,0 +1,20 @@
+<?php
+
+class Film{
+
+        private $id;
+        private $title;
+        private $genre;
+        private $releaseDate;
+        private $duration;
+
+        public function showfilms(){
+
+        }
+
+        public function totalfilms(){
+
+        }
+    
+
+}
