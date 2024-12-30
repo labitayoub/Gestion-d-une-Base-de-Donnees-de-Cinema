4b7c41d4abe9f1493d9e2677377c795a289d226f
labitayoub <ayoub.labite@gmail.com>
2024-12-30 04:02:49 +0100

    update class film

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
-require_once 'src/Admin.php';
+require_once 'src/Film.php';
+
+$Film1 = new Film(id :1,titre :"ayoub",genre :"action",duree :'1:00 H', date_sortie :"2023/02/03", realisation :"testt",distribution :"test");
+
+
+
+
+// $Film1->titre = "hassan";
+
+
+$Film1->setTitre(titre:'hassan');
+
+
+echo $Film1->__Tostring();
+
+
+// echo $Film1->getId();
+// echo $Film1->getTitre();
+// echo $Film1->getGenre();
+// echo $Film1->getDuree();
+// echo $Film1->getDate_sortie();
+// echo $Film1->getRealisation();
+// echo $Film1->getDistribution();
+
--- src/Film.php
+++ src/Film.php
@@ -1,24 +1,95 @@
 <?php
 
-class Film{
+class Film
+{
 
-        private $id;
-        private $titre;
-        private $genre;
-        private $duree;
-        private $date_sortie;
-        private $realisation;
-        private $distribution;
+    private $id;
+    private $titre;
+    private $genre;
+    private $duree;
+    private $date_sortie;
+    private $realisation;
+    private $distribution;
 
-        public function __construct($titre,$genre,$duree, $date_sortie, $realisation,$distribution,) {
-            $this->titre = $titre;
-            $this->genre = $genre;
-            $this->duree = $duree;
-            $this->date_sortie = $date_sortie;
-            $this->realisation = $realisation;
-            $this->distribution = $distribution;
-        }
+    public function __construct($id, $titre, $genre, $duree, $date_sortie, $realisation, $distribution,)
+    {
+        $this->id = $id;
+        $this->titre = $titre;
+        $this->genre = $genre;
+        $this->duree = $duree;
+        $this->date_sortie = $date_sortie;
+        $this->realisation = $realisation;
+        $this->distribution = $distribution;
+    }
 
-    
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+    public function getDate_sortie()
+    {
+        return $this->date_sortie;
+    }
+    public function getRealisation()
+    {
+        return $this->realisation;
+    }
+    public function getDistribution()
+    {
+        return $this->distribution;
+    }
 
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+    }
+    public function setDuree($duree)
+    {
+        $this->duree = $duree;
+    }
+    public function setDate_sortie($date_sortie)
+    {
+        $this->date_sortie = $date_sortie;
+    }
+    public function setRealisation($realisation)
+    {
+        $this->realisation = $realisation;
+    }
+    public function setDistribution($distribution)
+    {
+        $this->distribution = $distribution;
+    }
+
+
+    public function __toString()
+    {
+        return "id :" . $this->id .
+            ", titre :" . $this->titre .
+            ", genre :" . $this->genre .
+            ", duree :" . $this->duree .
+            ", date_sortie :" . $this->date_sortie .
+            ", date_sortie :" . $this->realisation .
+            ", distribution :" . $this->distribution;
+    }
 }
